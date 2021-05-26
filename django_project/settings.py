07df4eca8452a4e60d7a12ad794bb672c0c5bd83
= <=>
2021-05-26 20:24:38 +0300

    Updated Settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yqf9we)v+e8l*guo8o3l+^f%2)3+c(v%%0&yl^4m!0m4d@^)pu'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
